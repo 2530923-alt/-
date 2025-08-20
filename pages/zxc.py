7be8fe77fbe18bdc5d1812e774b8568b2af109ea
2530923-alt <2530923@sw.hs.kr>
2025-08-20 14:38:47 +0900

    Update zxc.py

--- pages/zxc.py
+++ pages/zxc.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
-st.title('김준 R.I.P')
+st.title('공식')
 
-# st.tabs(["이름1", "이름2", ...]): 탭 인터페이스 생성
-tab1, tab2 = st.tabs(["탭 1", "탭 2"])  # 2개의 탭 생성
+
+tab1, tab2 = st.tabs(["수학", "물리"])  # 2개의 탭 생성
 
 with tab1:
-    st.write("탭 1에 해당하는 내용입니다.")  # 첫 번째 탭에 표시할 내용
+    mbti = st.selectbox('MBTI를 선택해주세요:', [
+    'INTJ', 'INTP', 'ENTJ', 'ENTP', 'INFJ', 'INFP', 'ENFJ', 'ENFP', 
+    'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ', 'ISTP', 'ISFP', 'ESTP', 'ESFP'
+])
+
+    
 with tab2:
     st.write("탭 2에 해당하는 내용입니다.")  # 두 번째 탭에 표시할 내용
 

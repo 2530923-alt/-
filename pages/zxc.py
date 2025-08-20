22cbf1b4d0d781c9285b04b9595d869dc6f5c66d
2530923-alt <2530923@sw.hs.kr>
2025-08-20 14:32:50 +0900

    Update zxc.py

--- pages/zxc.py
+++ pages/zxc.py
@@ -4,11 +4,7 @@ st.title('김준 R.I.P')
 
 st.sidebar.title("📌 사이드바 메뉴")
 option = st.sidebar.selectbox("옵션을 선택하세요", ["A", "B"])
-'''
-with st.sidebar.selectbox([A]):
-    name = st.text_input('이름을 입력해주세요 : ')
-    mbti = st.selectbox('MBTI를 선택해주세요:', ['INTJ', 'INTP', 'ENTJ', 'ENTP', 'INFJ', 'INFP', 'ENFJ', 'ENFP', 'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ', 'ISTP', 'ISFP', 'ESTP', 'ESFP'])
-'''
+
 
 
 

1abcb5af27db6df54a8d06e265b5f3c4b8b1e9c3
2530923-alt <2530923@sw.hs.kr>
2025-08-20 14:29:31 +0900

    Update zxc.py

--- pages/zxc.py
+++ pages/zxc.py
@@ -6,12 +6,10 @@ st.sidebar.title("📌 사이드바 메뉴")
 option = st.sidebar.selectbox("옵션을 선택하세요", ["A", "B"])
 st.write("선택한 옵션:", option)
 
-with A:
-  name = st.text_input('이름을 입력해주세요 : ')
-  mbti = st.selectbox('MBTI를 선택해주세요:', [
-    'INTJ', 'INTP', 'ENTJ', 'ENTP', 'INFJ', 'INFP', 'ENFJ', 'ENFP', 
-    'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ', 'ISTP', 'ISFP', 'ESTP', 'ESFP'
-])
+with option A:
+    name = st.text_input('이름을 입력해주세요 : ')
+    mbti = st.selectbox('MBTI를 선택해주세요:', ['INTJ', 'INTP', 'ENTJ', 'ENTP', 'INFJ', 'INFP', 'ENFJ', 'ENFP', 'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ', 'ISTP', 'ISFP', 'ESTP', 'ESFP'])
+
 
 
 

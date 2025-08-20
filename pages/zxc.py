67c0c60dce54b0c109e7d9b7dfb655b6e458b193
2530923-alt <2530923@sw.hs.kr>
2025-08-20 14:21:11 +0900

    Update zxc.py

--- pages/zxc.py
+++ pages/zxc.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('ㄴㄱㅁ')
 
 st.sidebar.title("📌 사이드바 메뉴")
-option = st.sidebar.selectbox("옵션을 선택하세요", ["A", "B", "C"])
+option = st.sidebar.selectbox("옵션을 선택하세요", ["A", "B"])
 st.write("선택한 옵션:", option)
 
 

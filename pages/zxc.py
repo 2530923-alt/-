aee94f47b632e70d9cf4d066bdaa7bf772ed42cb
2530923-alt <2530923@sw.hs.kr>
2025-08-21 09:20:02 +0900

    Update zxc.py

--- pages/zxc.py
+++ pages/zxc.py
@@ -8,7 +8,7 @@ tab1, tab2 = st.tabs(["박채윤", "154"])  # 2개의 탭 생성
 with tab1:
     fuck = st.selectbox('선택해주세요:', [1])
 
-    fuck_data = {'1' : you{'r"E = mc^2"'}}
+    fuck_data = {'1' : {'r"E = mc^2"'}}
 
     if st.button('hhh'):
         if fuck in fuck_data:
